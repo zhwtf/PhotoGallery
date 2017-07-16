bc8bdb6be262c2b7cd43a063ef72d968647dadb8
haozheng <zhwtf123@gmail.com>
2017-07-16 15:45:58 -0700

    CH24 使用和传递handler

--- app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
@@ -1,7 +1,15 @@
 package com.bignerdranch.android.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by zhenghao on 2017-07-15.
@@ -9,12 +17,53 @@ import android.util.Log;
 
 public class ThumbnailDownloader<T> extends HandlerThread {
     private static final String TAG = "ThumbnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 0;
 
     private Boolean mHasQuit = false;
 
-    public ThumbnailDownloader() {
-        super(TAG);
+    private Handler mRequestHandler;
+    private ConcurrentMap<T,String> mRequestMap = new ConcurrentHashMap<>();
+    /*
+    MESSAGE_DOWNLOAD用来标识下载请求消息。（ThumbnailDownloader会把它设为任何新创
+建下载消息的what属性。）
+新添加的mRequestHandler用来存储对Handler的引用。这个Handler负责在Thumbnail
+Downloader后台线程上管理下载请求消息队列。这个Handler也负责从消息队列里取出并处理
+下载请求消息。
+新添加的mRequestMap是个ConcurrentHashMap。这是一种线程安全的HashMap。这里，使
+用一个标记下载请求的T类型对象作为key，我们可以存取和请求关联的URL下载链接。（这个标
+记对象是PhotoHolder，下载结果该发送给哪个显示图片的UI元素再明白不过了。）
+     */
 
+    private Handler mResponseHandler;
+    private ThumbnailDownloadListener<T> mThumbnailDownloadListener;
+
+    public interface ThumbnailDownloadListener<T> {
+        void onThumbnailDownloaded(T target, Bitmap thumbnail);
+    }
+
+    public void setThumbnailDownloadListener(ThumbnailDownloadListener<T> listener) {
+        mThumbnailDownloadListener = listener;
+    }
+
+
+    public ThumbnailDownloader(Handler responseHandler) {
+        super(TAG);
+        mResponseHandler = responseHandler;
+
+    }
+
+    @Override
+    protected void onLooperPrepared() {
+        mRequestHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    T target = (T) msg.obj;
+                    Log.i(TAG, "Got a request for URL: " + mRequestMap.get(target));
+                    handleRequest(target);
+                }
+            }
+        };
     }
 
     @Override
@@ -25,6 +74,45 @@ public class ThumbnailDownloader<T> extends HandlerThread {
 
     public void queueThumbnail(T target, String url) {
         Log.i(TAG, "Got a URL: " + url);
+
+        if (url == null) {
+            mRequestMap.remove(target);
+        } else {
+            mRequestMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget();
+        }
+    }
+
+    public void clearQueue() {
+        mRequestHandler.removeMessages(MESSAGE_DOWNLOAD);
+    }
+
+    private void handleRequest(final T target) {
+        try {
+            final String url = mRequestMap.get(target);
+
+            if (url == null) {
+                return;
+            }
+
+            byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+            final Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+
+            mResponseHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (mRequestMap.get(target) != url || mHasQuit){
+                        return;
+                    }
+                    mRequestMap.remove(target);
+                    mThumbnailDownloadListener.onThumbnailDownloaded(target, bitmap);
+                }
+            });
+
+        } catch(IOException ioe) {
+            Log.e(TAG, "Error downloading image", ioe);
+        }
     }
 
 }
